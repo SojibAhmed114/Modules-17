2c2bc098b8429569de58c6448df26b6477549e70
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-27 16:43:50 +0600

    if else added

---
+++ comparison.js
@@ -0,0 +1,49 @@
+// javadcript Comparison:
+    
+    // Bigger:  > (Greater  than:  বড়)  
+	// less:   <  (less than : ছোট) 
+	// Equal to:  ==   (সমান)
+	// Greater than or Equal to :  >=  ( বড় অথবা সমান )
+	// Less than to Equal to :  <=  (ছোট অথবা সমান)
+	// Not Equal :  !=  (সমান না অর্থাৎ বেশি না হয় কম)
+	// And :  &&  (একাধিক শর্ত । যেমন,   বিয়ে করতে হলে পাত্র কে বিসিএস ক্যাডারও হতে হবে আবার বড়লোকও হতে হবে।)
+	// Or: || (একাধিকের মধ্যে একটি শর্ত, যেমন; পাত্রকে হয় সরকারী চাকরিজীবি হতে হবে আর না হয় বড় ব্যাবসায়ী হতে হবে।)
+
+    
+// console.log(5 > 10);
+// console.log(10 > 10);
+// console.log(11 > 10);
+// console.log(11 < 10);
+//  console.log(9 < 10);
+//  console.log(9 >= 10);
+//  console.log(10 >= 10);
+//  console.log(100 >= 10);
+//  console.log(100 <= 10);
+//  console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(10 == 10);
+// console.log(10 == 10.8);
+
+var orange = 50;
+var banana = 100;
+// console.log(orange < banana);
+// console.log(banana > orange);
+// console.log(banana > orange);
+ 
+// console.log(10 == "sojib");
+// console.log(10 == "10");
+// console.log(10 === "10")
+
+// console.log(50 == "50");
+// console.log(50 === "50");
+
+
+// aita bujhi nai
+
+console.log(10 != 10); 
+
+console.log(10 != "10");
+
+console.log(10 !== "10");
+
+
---
+++ question.js
@@ -0,0 +1,51 @@
+//  question one
+
+// var a = isNaN("sojib ahemd");
+// console.log(a);    //true
+
+// var b = isNaN("141");
+// console.log(b);  //false
+
+// var c = isNaN(4 - "hello");
+// console.log(c);  //true
+
+
+// question two  //string to number convert
+// let x = '10';
+// let num = Number(x);
+// console.log(typeof num);   //outpur-10
+
+
+// var sakil = "100";
+// sakil = Number(sakil);
+// console.log(typeof sakil);
+
+
+// var pirce = "5410";
+// conver = Number(pirce);
+// console.log(typeof conver);
+
+
+
+// var sojib = "510";
+// sojib = parseInt(sojib);
+// console.log(typeof sojib);
+
+// var safi = 5104;
+// safi = "" + safi;
+// console.log(typeof safi);
+
+
+
+
+// console.log(200 != 200);
+// console.log(10 == 10);
+// console.log(500 != 400);
+
+// console.log(10 != "10");
+
+console.log(200 == "200");
+console.log(200 != "200")
+
+console.log(10 !== "10");
+

9fe59956038cce73c90f98ab412a9dcc97508d6a
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-28 10:43:22 +0600

    not

---
+++ tarnary-practice.js
@@ -0,0 +1,67 @@
+// jodi age 15 ar nicche hoy tahole free.kintu jodi car theke tahole free hobe na. 
+// age 60 ar beshi hole 50% discount but car thakle 10% discount.
+
+let price = 500;
+let age = 70;
+const isCar = false;
+
+if (age < 15 && isCar === false) {
+    console.log("you can eat for free");
+    if (age > 60 && isCar === false) {              //problem
+        let discount = price * 50 / 100;
+        let paybill = price - discount;
+        console.log(paybill);
+    }
+}
+
+else if (age > 60 && isCar) {
+    let discount = price * 10 / 100;
+    let paybill = price - discount;
+    console.log(paybill);
+}
+else (
+    console.log(price)
+)
+
+// tarnary:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (age < 15 && isCar === false) {
+//     console.log("you can eat for free");
+//     if (age > 60 && isCar === false) {              //problem
+//         let discount = price * 50 / 100;
+//         let paybill = price - discount;
+//         console.log(paybill);
+//     }
+//     else {
+//         (age > 60 && isCar) 
+//         let discount = price * 10 / 100;
+//         let paybill = price - discount;
+//         console.log(paybill);
+//     }
+// }
+ 
+// else (
+//     console.log(price)
+// )
---
+++ ternary-shorthanl.js
@@ -0,0 +1,27 @@
+// ternary - three part;
+
+
+
+
+// if (age >= 18) {
+//     console.log("you can vote")
+// }
+// else {
+//     console.log("tomake diye hobe na")
+// }
+// const age = 17;
+// age >= 18 ? console.log("you can vote") : console.log("fidar khau babu")
+
+// video 17.7
+
+// let price = 500;
+// const isLeader = true;
+
+// price = isLeader === false ? 0 : price + 100;
+// console.log(price);
+
+
+let money = 1000;
+const isTeamBoy = true;
+money = isTeamBoy === true ? money > 1000 ? money / 2 : 0 : money + 100;
+console.log(money);
